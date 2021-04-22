b29fcdbb1c45d2c6e7dd2aa0dfaeb05bbfaa8018
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-04-22 15:38:15 -0400

    Removes example Tag API endpoint

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from api.routes.ping import router as ping_router
-from api.routes.tag import router as tag_router
 
 router = APIRouter()
 
 router.include_router(ping_router, prefix="/ping", tags=["ping"])
-router.include_router(tag_router, prefix="/tag", tags=["tag"])
