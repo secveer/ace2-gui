5936c03f615a28e145d5dec40a2c924342648989
hollyfoxx <31867815+hollyfoxx@users.noreply.github.com>
2021-05-09 10:05:50 -0400

    Remove unused data from new components

--- frontend/app/src/components/TheAlertsTable.vue
+++ frontend/app/src/components/TheAlertsTable.vue
@@ -106,104 +106,31 @@ export default {
   data() {
     return {
       alertTableFilters: null,
-      chosenEvent: null,
       columns: [{field: 'alert_date', header: 'Alert Date'},
+        {field: 'name', header: 'Name'},
         {field: 'disposition', header: 'Disposition'},
+        {field: 'owner', header: 'Owner'},
+        {field: 'type', header: 'Type'},
         {field: 'disposition_by', header: 'Dispositioned By'},
         {field: 'event_date', header: 'Event Date'},
-        {field: 'name', header: 'Name'},
-        {field: 'owner', header: 'Owner'},
         {field: 'queue', header: 'Queue'},
         {field: 'remediated_by', header: 'Remediated By'},
         {field: 'remediated_date', header: 'Remediated Date'},
         {field: 'remediation_status', header: 'Remediation Status'},
-        {field: 'type', header: 'Type'},
       ],
-      displayAssignModal: false,
-      displayCommentModal: false,
-      displayConfirmation: false,
-      displayDispositionModal: false,
-      displayEditFilterModal: false,
-      displayRemediateModal: false,
-      displaySaveToEventModal: false,
-      displayTagModal: false,
-      dispositions: ['FALSE_POSITIVE', 'WEAPONIZATION', 'COMMAND_AND_CONTROL'],
-      elevated_dispositions: ['COMMAND_AND_CONTROL'],
-      endTimeFilterData: null,
-      existingEvents: [{'title': 'Open', 'events': ['event1', 'event2']},
-        {'title': 'Closed', 'events': ['event3', 'event4']}],
       expandedRows: [],
-      filteredTags: null,
-      newEventComment: null,
-      newEventName: null,
-      newTags: [],
-      queues: ['external', 'internal', 'intel'],
-      remediationStatuses: ['remediated', 'remediation_failed', 'remediating'],
-      remediationTargets: [
-        {
-          'type': 'zerofox',
-          'target': 'bad_url',
-          'status': 'remediation failed',
-          'id': '123'
-        },
-        {
-          'type': 'email',
-          'target': 'msgID|email.com',
-          'status': 'remediating',
-          'id': '124'
-        },
-        {
-          'type': 'email',
-          'target': 'msgID|email2.com',
-          'status': 'removed',
-          'id': '125'
-        },
-        {
-          'type': 'email',
-          'target': 'msgID|email3.com',
-          'status': 'restored',
-          'id': '126'
-        },
-      ],
       selectedColumns: null,
-      selectedOwners: null,
       selectedRows: null,
-      selectedRemediations: null,
-      startTimeFilterData: null,
-      suggestedComments: ['this is an old comment', 'and another'],
-      types: ['splunk_hunter'],
-      users: ['Holly', 'Analyst', 'none'],
     }
   },
   async created() {
     this.resetAlertTableFilters();
-  },
-  computed: {
-    showAddToEventButton: function () {
-      return this.elevated_dispositions.includes(this.appliedDisposition);
-    },
-    tags: function () {
-      let tags = [];
-      let unique_tags = [];
-      this.alerts.forEach(function(alert) {
-        tags = tags.concat(alert.tags);
-      });
-      tags.forEach((tag) => {
-        if (!unique_tags.includes(tag)) {
-          unique_tags.push(tag);
-        }
-      });
-      // todo: modify tags in alert object to be objects rather than a string
-      // this will allow for proper filtering
-      return unique_tags;
-    }
   },
   methods: {
     resetAlertTableFilters() {
       this.initAlertTableFilters();
       this.selectedColumns = this.columns.slice(0, 5);
     },
-
     initAlertTableFilters() {
       this.alertTableFilters = {
         'global': {value: null, matchMode: FilterMatchMode.CONTAINS}
@@ -212,62 +139,8 @@ export default {
     onToggle(value) {
       this.selectedColumns = this.columns.filter(col => value.includes(col));
     },
-    addExistingTag(event) {
-      this.newTags.push(event.value);
-    },
     exportCSV() {
       this.$refs.dt.exportCSV();
-    },
-    openDispositionModal() {
-      this.displayDispositionModal = true;
-    },
-    closeDispositionModal() {
-      this.displayDispositionModal = false;
-    },
-    openCommentModal() {
-      this.displayCommentModal = true;
-    },
-    closeCommentModal() {
-      this.displayCommentModal = false;
-    },
-    openAssignModal() {
-      this.displayAssignModal = true;
-    },
-    closeAssignModal() {
-      this.displayAssignModal = false;
-    },
-    openTagModal() {
-      this.displayTagModal = true;
-    },
-    closeTagModal() {
-      this.displayTagModal = false;
-    },
-    openRemediateModal() {
-      this.displayRemediateModal = true;
-    },
-    openEditFilterModal() {
-      this.displayEditFilterModal = true;
-    },
-    closeEditFilterModal() {
-      this.displayEditFilterModal = false;
-    },
-    openSaveToEventModal() {
-      this.displaySaveToEventModal = true;
-    },
-    closeSaveToEventModal() {
-      this.displaySaveToEventModal = false;
-    },
-    openConfirmation() {
-      this.displayConfirmation = true;
-    },
-    closeConfirmation() {
-      this.displayConfirmation = false;
-    },
-    autoSetEventName() {
-      this.newEventName = "this is a placeholder";
-    },
-    test(event) {
-      console.log(event);
     }
   }
 }
