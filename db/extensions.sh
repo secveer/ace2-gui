55dc0c495d0a866849855a0a9b947b5252eeb78b
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-09 17:16:28 -0400

    Creates the test database

--- db/extensions.sh
+++ db/extensions.sh
@@ -1,8 +1,8 @@
-# Enable the required extensions
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-create extension pg_trgm;
-create extension pgcrypto;
-EOF
+# Create the "ace" and "ace_test" databases and enable the required extensions
+for db in ace ace_test
+do
+    echo "SELECT 'CREATE DATABASE $db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
 
-# Create the "ace" database if it does not already exist
-echo "SELECT 'CREATE DATABASE ace' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ace')\gexec" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION IF NOT EXISTS pg_trgm"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION IF NOT EXISTS pgcrypto"
+done
