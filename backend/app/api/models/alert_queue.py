d776b3da1cc179322f344ed36473721a694dcc18
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-17 14:18:21 -0400

    Finishes Alert Queue Pydantic models

--- backend/app/api/models/alert_queue.py
+++ backend/app/api/models/alert_queue.py
@@ -2,14 +2,24 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 
-class AlertQueue(BaseModel):
+class AlertQueueBase(BaseModel):
     """Represents an alert queue used to filter alerts (typically by an analyst's job function)."""
 
     description: Optional[str] = Field(description="An optional human-readable description of the alert queue")
 
-    id: int = Field(description="The ID of the alert queue")
-
     value: str = Field(description="The value of the alert queue")
+
+
+class AlertQueueCreate(AlertQueueBase):
+    pass
+
+
+class AlertQueueRead(AlertQueueBase):
+    id: int = Field(description="The ID of the alert queue")
 
     class Config:
         orm_mode = True
+
+
+class AlertQueueUpdate(AlertQueueBase):
+    value: Optional[str] = Field(description="The value of the alert queue")
