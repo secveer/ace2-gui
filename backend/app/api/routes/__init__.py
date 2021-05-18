aa4a566d1478ed6dfc081648fe40ef35f8036abc
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-18 16:26:03 -0400

    Adds AlertType API routes

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from api.routes.alert import router as alert_router
 from api.routes.alert_queue import router as alert_queue_router
+from api.routes.alert_type import router as alert_type_router
 from api.routes.disposition import router as disposition_router
 from api.routes.ping import router as ping_router
 
@@ -10,5 +11,6 @@ router = APIRouter()
 
 router.include_router(alert_router)
 router.include_router(alert_queue_router)
+router.include_router(alert_type_router)
 router.include_router(disposition_router)
 router.include_router(ping_router)
