632072ec3376dcd3ec1c9aeb505caaef05ddc336
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-18 16:25:43 -0400

    Completes AlertType Pydantic models

--- backend/app/api/models/alert_type.py
+++ backend/app/api/models/alert_type.py
@@ -1,15 +1,28 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from uuid import UUID
 
 
-class AlertType(BaseModel):
+class AlertTypeBase(BaseModel):
     """Represents a type of alert."""
 
     description: Optional[str] = Field(description="An optional human-readable description of the alert type")
 
-    id: int = Field(description="The ID of the alert type")
+    uuid: Optional[UUID] = Field(description="The UUID of the alert type")
 
     value: str = Field(description="The value of the alert type")
 
+
+class AlertTypeCreate(AlertTypeBase):
+    pass
+
+
+class AlertTypeRead(AlertTypeBase):
+    uuid: UUID = Field(description="The UUID of the alert type")
+
     class Config:
         orm_mode = True
+
+
+class AlertTypeUpdate(AlertTypeBase):
+    value: Optional[str] = Field(description="The value of the alert type")
