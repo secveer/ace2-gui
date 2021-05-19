87f93dbc54d2cc10c8553c930b0b9600df9eaa86
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-19 15:31:31 -0400

    Adds NodeThreatActor API routes

---
+++ backend/app/api/routes/node_threat_actor.py
@@ -0,0 +1,84 @@
+from fastapi import APIRouter, Depends, Request, Response
+from sqlalchemy.orm import Session
+from typing import List
+from uuid import UUID
+
+from api.models.node_threat_actor import NodeThreatActorCreate, NodeThreatActorRead, NodeThreatActorUpdate
+from api.routes import helpers
+from db import crud
+from db.database import get_db
+from db.schemas.node_threat_actor import NodeThreatActor
+
+
+router = APIRouter(
+    prefix="/node/threat_actor",
+    tags=["Node Threat Actor"],
+)
+
+
+#
+# CREATE
+#
+
+
+def create_node_threat_actor(
+    node_threat_actor: NodeThreatActorCreate,
+    request: Request,
+    response: Response,
+    db: Session = Depends(get_db),
+):
+    uuid = crud.create(obj=node_threat_actor, db_table=NodeThreatActor, db=db)
+
+    response.headers["Content-Location"] = request.url_for("get_node_threat_actor", uuid=uuid)
+
+
+helpers.api_route_create(router, create_node_threat_actor)
+
+
+#
+# READ
+#
+
+
+def get_all_node_threat_actors(db: Session = Depends(get_db)):
+    return crud.read_all(db_table=NodeThreatActor, db=db)
+
+
+def get_node_threat_actor(uuid: UUID, db: Session = Depends(get_db)):
+    return crud.read(uuid=uuid, db_table=NodeThreatActor, db=db)
+
+
+helpers.api_route_read_all(router, get_all_node_threat_actors, List[NodeThreatActorRead])
+helpers.api_route_read(router, get_node_threat_actor, NodeThreatActorRead)
+
+
+#
+# UPDATE
+#
+
+
+def update_node_threat_actor(
+    uuid: UUID,
+    node_threat_actor: NodeThreatActorUpdate,
+    request: Request,
+    response: Response,
+    db: Session = Depends(get_db),
+):
+    crud.update(uuid=uuid, obj=node_threat_actor, db_table=NodeThreatActor, db=db)
+
+    response.headers["Content-Location"] = request.url_for("get_node_threat_actor", uuid=uuid)
+
+
+helpers.api_route_update(router, update_node_threat_actor)
+
+
+#
+# DELETE
+#
+
+
+def delete_node_threat_actor(uuid: UUID, db: Session = Depends(get_db)):
+    crud.delete(uuid=uuid, db_table=NodeThreatActor, db=db)
+
+
+helpers.api_route_delete(router, delete_node_threat_actor)
