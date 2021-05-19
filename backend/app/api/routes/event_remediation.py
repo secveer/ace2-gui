b1065b6dfc86aa09f55bd93eb1a550cf38238446
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-18 21:25:25 -0400

    Adds EventRemediation API endpoints

---
+++ backend/app/api/routes/event_remediation.py
@@ -0,0 +1,84 @@
+from fastapi import APIRouter, Depends, Request, Response
+from sqlalchemy.orm import Session
+from typing import List
+from uuid import UUID
+
+from api.models.event_remediation import EventRemediationCreate, EventRemediationRead, EventRemediationUpdate
+from api.routes import helpers
+from db import crud
+from db.database import get_db
+from db.schemas.event_remediation import EventRemediation
+
+
+router = APIRouter(
+    prefix="/event/remediation",
+    tags=["Event Remediation"],
+)
+
+
+#
+# CREATE
+#
+
+
+def create_event_remediation(
+    event_remediation: EventRemediationCreate,
+    request: Request,
+    response: Response,
+    db: Session = Depends(get_db),
+):
+    uuid = crud.create(obj=event_remediation, db_table=EventRemediation, db=db)
+
+    response.headers["Content-Location"] = request.url_for("get_event_remediation", uuid=uuid)
+
+
+helpers.api_route_create(router, create_event_remediation)
+
+
+#
+# READ
+#
+
+
+def get_all_event_remediations(db: Session = Depends(get_db)):
+    return crud.read_all(db_table=EventRemediation, db=db)
+
+
+def get_event_remediation(uuid: UUID, db: Session = Depends(get_db)):
+    return crud.read(uuid=uuid, db_table=EventRemediation, db=db)
+
+
+helpers.api_route_read_all(router, get_all_event_remediations, List[EventRemediationRead])
+helpers.api_route_read(router, get_event_remediation, EventRemediationRead)
+
+
+#
+# UPDATE
+#
+
+
+def update_event_remediation(
+    uuid: UUID,
+    event_remediation: EventRemediationUpdate,
+    request: Request,
+    response: Response,
+    db: Session = Depends(get_db),
+):
+    crud.update(uuid=uuid, obj=event_remediation, db_table=EventRemediation, db=db)
+
+    response.headers["Content-Location"] = request.url_for("get_event_remediation", uuid=uuid)
+
+
+helpers.api_route_update(router, update_event_remediation)
+
+
+#
+# DELETE
+#
+
+
+def delete_event_remediation(uuid: UUID, db: Session = Depends(get_db)):
+    crud.delete(uuid=uuid, db_table=EventRemediation, db=db)
+
+
+helpers.api_route_delete(router, delete_event_remediation)
