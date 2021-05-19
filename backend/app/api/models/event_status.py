452f0853f93274a879ff191e1cdbac8ac8c2e408
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-19 10:09:56 -0400

    Completes EventStatus Pydantic models

--- backend/app/api/models/event_status.py
+++ backend/app/api/models/event_status.py
@@ -1,15 +1,28 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from uuid import UUID
 
 
-class EventStatus(BaseModel):
+class EventStatusBase(BaseModel):
     """Represents a status that can be applied to an event (open, closed, etc)."""
 
-    description: Optional[str] = Field(description="An optional human-readable description of the status")
+    description: Optional[str] = Field(description="An optional human-readable description of the event status")
 
-    id: int = Field(description="The ID of the status")
+    uuid: Optional[UUID] = Field(description="The UUID of the event status")
 
-    value: str = Field(description="The value of the status")
+    value: str = Field(description="The value of the event status")
+
+
+class EventStatusCreate(EventStatusBase):
+    pass
+
+
+class EventStatusRead(EventStatusBase):
+    uuid: UUID = Field(description="The UUID of the event status")
 
     class Config:
         orm_mode = True
+
+
+class EventStatusUpdate(EventStatusBase):
+    value: Optional[str] = Field(description="The value of the event status")
