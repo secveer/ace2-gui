9e220c1a83724d55785dc02148a917fa080539db
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-19 15:32:38 -0400

    Renames ThreatActor to NodeThreatActor for more clarity

--- backend/app/db/schemas/node_threat_actor.py
+++ backend/app/db/schemas/node_threat_actor.py
@@ -4,8 +4,8 @@ from sqlalchemy.dialects.postgresql import UUID
 from db.database import Base
 
 
-class ThreatActor(Base):
-    __tablename__ = "threat_actor"
+class NodeThreatActor(Base):
+    __tablename__ = "node_threat_actor"
 
     uuid = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
 
