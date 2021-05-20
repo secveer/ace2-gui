43c62afdc926d19fae2bf1ae29a7c08046da0dec
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-20 08:55:18 -0400

    Adds event_source database table

---
+++ backend/app/db/schemas/event_source.py
@@ -0,0 +1,14 @@
+from sqlalchemy import func, Column, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from db.database import Base
+
+
+class EventSource(Base):
+    __tablename__ = "event_source"
+
+    uuid = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
+
+    description = Column(String)
+
+    value = Column(String, nullable=False, unique=True, index=True)
