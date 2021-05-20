c98d6b2d7d09577eebc7083d55af476a869a816a
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-20 15:21:23 -0400

    Completes NodeThreat Pydantic models

---
+++ backend/app/api/models/node_threat.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Set
+from uuid import UUID
+
+from api.models.node_threat_type import NodeThreatTypeRead
+
+
+class NodeThreatBase(BaseModel):
+    """Represents a threat that can be applied to a node to denote things like a family of malware or specific type of attack."""
+
+    description: Optional[str] = Field(description="An optional human-readable description of the threat")
+
+    types: Set[str] = Field(description="A list of types the threat represents")
+
+    uuid: Optional[UUID] = Field(description="The UUID of the threat")
+
+    value: str = Field(description="The value of the threat")
+
+
+class NodeThreatCreate(NodeThreatBase):
+    pass
+
+
+class NodeThreatRead(NodeThreatBase):
+    types: List[NodeThreatTypeRead] = Field(description="A list of types the threat represents")
+
+    uuid: UUID = Field(description="The UUID of the threat")
+
+    class Config:
+        orm_mode = True
+
+
+class NodeThreatUpdate(NodeThreatBase):
+    types: Optional[Set[str]] = Field(description="A list of types the threat represents")
+
+    value: Optional[str] = Field(description="The value of the threat")
