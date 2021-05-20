8d604e471e03918d5fd29d88cb675f8d97768138
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-20 15:22:57 -0400

    Renames Threat to NodeThreat for more clarity

--- backend/app/db/schemas/node_threat.py
+++ backend/app/db/schemas/node_threat.py
@@ -6,8 +6,8 @@ from db.database import Base
 from db.schemas.node_threat_node_threat_type_mapping import node_threat_node_threat_type_mapping
 
 
-class Threat(Base):
-    __tablename__ = "threat"
+class NodeThreat(Base):
+    __tablename__ = "node_threat"
 
     uuid = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
 
