b7a9ec0e34ee876c3d6bc91ec51158012a5d1772
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-05-22 00:52:01 -0400

    Adds optional argument to specify tests to run

--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose-dev.yml run -e TESTING=1 backend pytest -vv
+docker-compose -f docker-compose-dev.yml run -e TESTING=1 backend pytest "$1" -vv
