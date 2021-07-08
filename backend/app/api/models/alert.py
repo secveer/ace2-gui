6c5cbefac8068c1583f969b9f1a2bc9650237ccb
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-07-08 14:10:08 -0400

    Adds Alert Pydantic models

--- backend/app/api/models/alert.py
+++ backend/app/api/models/alert.py
@@ -1,40 +1,97 @@
 from datetime import datetime
-from pydantic import Field, UUID4
+from pydantic import Field
 from typing import Optional
+from uuid import UUID, uuid4
 
-from api.models.alert_queue import AlertQueue
-from api.models.alert_type import AlertType
-from api.models.analysis import Analysis
-from api.models.disposition import Disposition
-from api.models.user import User
+from api.models import type_str, validators
+from api.models.alert_disposition import AlertDispositionRead
+from api.models.alert_queue import AlertQueueRead
+from api.models.alert_tool import AlertToolRead
+from api.models.alert_tool_instance import AlertToolInstanceRead
+from api.models.alert_type import AlertTypeRead
+from api.models.node import NodeBase, NodeCreate, NodeRead, NodeUpdate
+from api.models.user import UserRead
 
 
-class Alert(Analysis):
-    """Represents a RootAnalysis from the core that has at least one detection point."""
+class AlertBase(NodeBase):
+    """Represents an alert, which is a RootAnalysis from the ACE Core or one manually created by an analyst."""
 
-    alert_type: AlertType = Field(description="The type of this alert")
+    description: Optional[type_str] = Field(description="A short description of the alert")
 
-    disposition: Optional[Disposition] = Field(description="The disposition assigned to this alert")
+    event_time: datetime = Field(
+        default_factory=datetime.utcnow,
+        description="The time the activity alerted on occurred"
+    )
+
+    instructions: Optional[type_str] = Field(
+        description="""An optional human readable list of instructions that an analyst should perform when manually
+            reviewing this alert."""
+    )
+
+    name: Optional[type_str] = Field(
+        description="""An optional property that defines a name for an alert. Used to track and document analyst
+            response instructions."""
+    )
+
+    # TODO: When we have authentication, creating a manual alert will infer the owner from the token.
+    owner: Optional[type_str] = Field(description="The username of the user who has taken ownership of this alert")
+
+    queue: type_str = Field(description="The alert queue containing this alert")
+
+    tool: Optional[type_str] = Field(description="The tool that created this alert")
+
+    tool_instance: Optional[type_str] = Field(description="The instance of the tool that created this alert")
+
+    type: type_str = Field(description="The type of this alert")
+
+    uuid: UUID = Field(default_factory=uuid4, description="The UUID of the alert")
+
+
+class AlertCreate(NodeCreate, AlertBase):
+    pass
+
+
+class AlertRead(NodeRead, AlertBase):
+    disposition: Optional[AlertDispositionRead] = Field(
+        description="The disposition assigned to this alert"
+    )
 
     disposition_time: Optional[datetime] = Field(description="The time this alert was most recently dispositioned")
 
-    disposition_user: Optional[User] = Field(description="The user who most recently dispositioned this alert")
+    disposition_user: Optional[UserRead] = Field(description="The user who most recently dispositioned this alert")
 
-    event_uuid: Optional[UUID4] = Field(description="The unique ID of the event containing this alert")
+    event: Optional[UUID] = Field(description="The UUID of the event containing this alert")
 
     insert_time: datetime = Field(description="The time this alert was created")
 
-    mode: str = Field(description="The mode under which this alert is currently operating")
+    owner: Optional[UserRead] = Field(description="The user who has taken ownership of this alert")
 
-    owner: Optional[User] = Field(description="The user who has taken ownership of this alert")
+    queue: AlertQueueRead = Field(description="The alert queue containing this alert")
 
-    queue: AlertQueue = Field(description="The alert queue containing this alert")
+    tool: Optional[AlertToolRead] = Field(description="The tool that created this alert")
 
-    tool: str = Field(description="The tool that created this alert")
+    tool_instance: Optional[AlertToolInstanceRead] = Field(
+        description="The instance of the tool that created this alert"
+    )
 
-    tool_instance: str = Field(description="The instance of the tool that created this alert")
-
-    version: UUID4 = Field(description="The version of this alert")
+    type: AlertTypeRead = Field(description="The type of this alert")
 
     class Config:
         orm_mode = True
+
+
+class AlertUpdate(NodeUpdate, AlertBase):
+    disposition: Optional[type_str] = Field(description="The disposition assigned to this alert")
+
+    # TODO: This should not be editable. When we have authentication in place, the user will be inferred from the token.
+    # disposition_user: Optional[type_str] = Field(
+    #     description="The username of the user who most recently dispositioned this alert"
+    # )
+
+    event: Optional[UUID] = Field(description="The UUID of the event containing this alert")
+
+    queue: Optional[type_str] = Field(description="The alert queue containing this alert")
+
+    type: Optional[type_str] = Field(description="The type of this alert")
+
+    _prevent_none: classmethod = validators.prevent_none("queue", "type")
