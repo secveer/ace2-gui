a10f75ce6129174bca9ffe0ee1a83c19b8504c99
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-07-08 14:12:53 -0400

    Fixes various issues with Alert db schema

--- backend/app/db/schemas/alert.py
+++ backend/app/db/schemas/alert.py
@@ -2,36 +2,42 @@ from sqlalchemy import Column, DateTime, ForeignKey, Index, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
-from db.schemas.analysis import Analysis
+from db.schemas.node import Node
 from db.schemas.helpers import utcnow
 
 
-class Alert(Analysis):
+class Alert(Node):
     __tablename__ = "alert"
 
-    uuid = Column(UUID(as_uuid=True), ForeignKey("analysis.uuid"), primary_key=True)
+    uuid = Column(UUID(as_uuid=True), ForeignKey("node.uuid"), primary_key=True)
 
-    alert_type = relationship("AlertType")
+    analysis_uuid = Column(UUID(as_uuid=True), ForeignKey("analysis.uuid"), nullable=False, unique=True)
 
-    alert_type_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_type.uuid"))
+    analysis = relationship("Analysis", foreign_keys=[analysis_uuid])
+
+    description = Column(String)
 
     disposition = relationship("AlertDisposition")
 
     disposition_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_disposition.uuid"))
 
-    disposition_time = Column(DateTime)
+    disposition_time = Column(DateTime(timezone=True))
 
     disposition_user_uuid = Column(UUID(as_uuid=True), ForeignKey("user.uuid"))
 
     disposition_user = relationship("User", foreign_keys=[disposition_user_uuid])
 
+    event_time = Column(DateTime(timezone=True), server_default=utcnow(), nullable=False)
+
     event_uuid = Column(UUID(as_uuid=True), ForeignKey("event.uuid"))
 
     event = relationship("Event", foreign_keys=[event_uuid])
 
-    insert_time = Column(DateTime, server_default=utcnow())
+    insert_time = Column(DateTime(timezone=True), server_default=utcnow(), nullable=False)
 
-    mode = Column(String)
+    instructions = Column(String)
+
+    name = Column(String)
 
     owner_uuid = Column(UUID(as_uuid=True), ForeignKey("user.uuid"))
 
@@ -39,16 +45,23 @@ class Alert(Analysis):
 
     queue = relationship("AlertQueue")
 
-    queue_id = Column(UUID(as_uuid=True), ForeignKey("alert_queue.uuid"))
+    queue_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_queue.uuid"), nullable=False)
 
-    tool = Column(String)
+    tool = relationship("AlertTool")
 
-    tool_instance = Column(String)
+    tool_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_tool.uuid"))
 
-    version = Column(UUID(as_uuid=True))
+    tool_instance = relationship("AlertToolInstance")
+
+    tool_instance_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_tool_instance.uuid"))
+
+    type = relationship("AlertType")
+
+    type_uuid = Column(UUID(as_uuid=True), ForeignKey("alert_type.uuid"), nullable=False)
 
     __mapper_args__ = {
         "polymorphic_identity": "alert",
     }
 
+    # TODO: We need a lot more indices here for when we introduce searching API endpoints.
     __table_args__ = (Index("event_uuid", event_uuid),)
